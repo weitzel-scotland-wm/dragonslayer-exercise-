cf71b279e17eeabdbc8a08f75e8530761bbe0d15
test <session2@START-Coding-MBP-C02PLK6KG8WN.local>
2015-09-29 17:56:39 -0700

    Added a switch statement to the beginning to ask which dragon the user would like to fight.

--- dragonslayer-excercise-.js
+++ dragonslayer-excercise-.js
@@ -31,6 +31,22 @@ var totalDamage = totalDamage + damageThisRound
 
 
 //NEW CODE
+var dragonFight = prompt("Which type of dragon will you face? The ICE dragon, the FIRE dragon, or the DARK dragon?");
+
+switch(dragonFight){
+    case 'ice':
+        alert("You crawl into the frozen cave to face the massive, white dragon. It rears its head as you get ready to strike.");
+        break;
+    case 'fire':
+        alert("You wipe the beads of sweat off your forehead as you enter the fiery depths of Mt. Flame. The dragon notices you in his lair and stands up tall, ready to roast you into crispy human meat.");
+        break;
+    case 'dark':
+        alert("You can't see anything as you enter the portal to the nether dimension. You hear a sound to your right and you get ready for the hardest fight of your life.");
+        break;
+    default:
+        alert("That is neither 'ice', 'fire', or 'dark'.");
+}
+
 while(slaying){
 
 
@@ -38,7 +54,7 @@ while(slaying){
         console.log("Wow...You actually hit that thing!")
         totalDamage = totalDamage += damageThisRound
         if(totalDamage >= 20){
-            console.log("Congratulations, you have ended the tyranny of the rainbow dragon that was protecting its nest. Do you feel good now?");
+            console.log("Congratulations, you have ended the tyranny of the dragon that was protecting its nest. Do you feel good now?");
             slaying = false
         }
         else if(totalDamage >= 5){
